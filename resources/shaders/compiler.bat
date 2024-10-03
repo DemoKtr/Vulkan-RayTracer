95eaefdc0e9d51597c5e97d80a203d26aa03832d
Piotr Mroczek <80900800+DemoKtr@users.noreply.github.com>
2024-10-03 18:25:26 +0200

    prepare for raytracer

---
+++ resources/shaders/compiler.bat
@@ -0,0 +1,8 @@
+glslangValidator -V VertexShader.vert -o vert.spv
+
+glslangValidator -V particleComputeShader.comp -o particleCompute.spv
+
+glslangValidator -V PixelShader.frag -o frag.spv
+
+
+pause
