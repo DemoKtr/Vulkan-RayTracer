6d9c4e8d9e99ed92c154103a6f15e9d9697e3bce
Piotr Mroczek <80900800+DemoKtr@users.noreply.github.com>
2024-12-28 13:00:46 +0100

    rendering Flags

---
+++ include/View/RenderingFlags.h
@@ -0,0 +1,10 @@
+#pragma once
+
+constexpr uint8_t FLAG_RENDER_DEFERED_RENDERING = 1 << 0; // bit 0
+constexpr uint8_t FLAG_RENDER_SKYBOX = 1 << 1; // bit 1
+constexpr uint8_t FLAG_RENDER_LIGHTS = 1 << 2;   // bit 2
+constexpr uint8_t FLAG_RENDER_TEST_ONE = 1 << 3;    // bit 3
+constexpr uint8_t FLAG_RENDER_TEST_TWO = 1 << 4;    // bit 4
+constexpr uint8_t FLAG_RENDER_TEST_THREE = 1 << 5;    // bit 5
+constexpr uint8_t FLAG_RENDER_TEST_FOUR = 1 << 6;    // bit 6
+constexpr uint8_t FLAG_RENDER_TEST_FIVE = 1 << 7;    // bit 7
